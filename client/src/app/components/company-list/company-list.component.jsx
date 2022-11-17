4f9aebf485866651655ecd922c899f9653139370
Chernomorova <chernomorova.lidiya@gmail.com>
2022-11-17 11:08:46 +0300

    update table due to reqs

--- client/src/app/components/company-list/company-list.component.jsx
+++ client/src/app/components/company-list/company-list.component.jsx
@@ -9,16 +9,13 @@ import Paper from "@mui/material/Paper";
 import CompanyItem from "../company-item/company-item.component";
 
 function CompanyList({ companies }) {
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+  function createData(asset, name, date, note) {
+    return { asset, name, date, note };
   }
 
   const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
+    createData("GOOG", 'Alphabet Inc', 'some date', 'note ololo'),
+
   ];
 
   return (
@@ -38,26 +35,24 @@ function CompanyList({ companies }) {
         >
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+              <TableCell>ASSET</TableCell>
+              <TableCell align="right">NAME</TableCell>
+              <TableCell align="right">DATE</TableCell>
+              <TableCell align="right">NOTE</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.asset}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.asset}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+                <TableCell align="right">{row.note}</TableCell>
               </TableRow>
             ))}
           </TableBody>
